4fed5db3421de4fd0429d12724f90720637ae53d
Aakash Srivastava <aakash013srivastava@gmail.com>
2017-03-17 16:35:31 +0530

    Adding google oauth config

---
+++ server/accounts.js
@@ -0,0 +1,10 @@
+ServiceConfiguration.configurations.remove({
+	service: "google"
+});
+
+ServiceConfiguration.configurations.insert({
+	service: "google",
+	clientId: "905572387563-o76r1tmho2d8b3ra8v2m8mtlca9l4rd9.apps.googleusercontent.com",
+	secret: "bL8Ng293IVYiDBzJbqjswBpg"
+	
+});

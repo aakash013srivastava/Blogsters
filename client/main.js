1011adff73fc2fbfbbf7b3707930b95f99997347
Aakash Srivastava <aakash013srivastava@gmail.com>
2017-03-20 14:09:58 +0530

    Rectified errors in posts insertion

--- client/main.js
+++ client/main.js
@@ -1,9 +1,12 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-
+import {Mongo} from 'meteor/mongo';
+import {Posts} from '../imports/posts.js';
 import './main.html';
 
-Template.navigation.events({
+
+
+	Template.navigation.events({
 	'click .logout':function(event){
 		event.preventDefault();
 		Meteor.logout();
@@ -17,10 +20,39 @@ Template.navigation.events({
 			if(err)
 				console.log(err);
 			else
-				console.log("No error!:"+Meteor.user().email);
+				console.log("No error!:");
 		});
 	}
 });
+
+	Template.myPosts.events({
+		'click #addPost':function(event){
+			event.preventDefault();
+			
+			var user_id = Meteor.userId();
+			var title = $('#title').val();
+			var category = $('#category').val();
+			console.log("addpost called"+category);
+			var content = $('#content').val();
+			var createdAt = new Date();
+			var updatedAt = new Date();
+			Meteor.call('addPost',user_id,title,category,content,createdAt,updatedAt,function(err){
+				if(err) 
+					console.log(err);
+				else 
+					console.log("No error upon insert");
+			});
+			console.log("Number of posts: "+Meteor.call('postsCount'));
+
+			
+		}
+	});
+
+
+if(Meteor.isServer){
+
+
+}
 
 
 /*  Route configuration for the application */
--- server/main.js
+++ server/main.js
@@ -1,4 +1,30 @@
 import { Meteor } from 'meteor/meteor';
+import {Posts} from '../imports/posts.js';
+
+/* Define methods to call from client */
+
+Meteor.methods({
+	'addPost': function(user_id,title,category,content,createdAt,updatedAt){
+		Posts.insert({
+		user_id: user_id,
+		category: category,
+		title:  title,
+		content: content,
+		createdAt: createdAt,
+		updatedAt:updatedAt,
+		});
+	},
+	'deletePost': function(id){
+		Posts.remove(id);
+	},
+	'updatePost':function(id,title,content,category){
+		//Posts.update(id,{$set:{title:title,content:content,category:category,updatedAt:new Date()}});
+	},
+	'postsCount': function(){
+		return Posts.findOne();
+	}
+});
+
 
 Meteor.startup(() => {
   // code to run on server at startup
